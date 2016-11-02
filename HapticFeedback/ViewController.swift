95d6684a9bfa86b3ab1becba64fb098f949143df
zachsimone <Zach@zachs-macbook-pro.local>
2016-11-02 20:58:47 +1100

    second commit

--- HapticFeedback/ViewController.swift
+++ HapticFeedback/ViewController.swift
@@ -59,7 +59,8 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+        //
     }
 
 
