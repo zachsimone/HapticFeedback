712a57d7c9aa16527e18167b4779abd4964d5266
zachsimone <Zach@zachs-macbook-pro.local>
2016-11-02 20:44:53 +1100

    Initial setup

--- HapticFeedback/ViewController.swift
+++ HapticFeedback/ViewController.swift
@@ -13,7 +13,49 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let prepare = UIFeedbackGenerator()
+        prepare.prepare()
     }
+    
+    @IBAction func error(_ sender: AnyObject) {
+        
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.error)
+    }
+    
+    @IBAction func success(_ sender: AnyObject) {
+        
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.success)
+    }
+    
+    @IBAction func warning(_ sender: AnyObject) {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.warning)
+    }
+    
+    @IBAction func lightImpact(_ sender: AnyObject) {
+        
+        let generator = UIImpactFeedbackGenerator(style: .light)
+        generator.impactOccurred()
+    }
+    
+    @IBAction func mediumImpact(_ sender: AnyObject) {
+        let generator = UIImpactFeedbackGenerator(style: .medium)
+        generator.impactOccurred()
+    }
+    
+    @IBAction func heavyImpact(_ sender: AnyObject) {
+        let generator = UIImpactFeedbackGenerator(style: .heavy)
+        generator.impactOccurred()
+    }
+    
+    @IBAction func other(_ sender: AnyObject) {
+        let generator = UISelectionFeedbackGenerator()
+        generator.selectionChanged()
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
